00a470d34fdf2ec430d2bed5f3f5d4eb5cbc9308
Teja vardhan Vemulapalli <tejavardhan8978@gmail.com>
2025-09-28 15:16:12 -0500

    Initial commit

---
+++ src/test/java/edu/metro/grocerystore/GroceryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.metro.grocerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
